0c835f7bdfeaa2f5397dd7524d4bc4fb938d342c
Aleksander Bang-Larsen <github@aleksanderbl.dk>
2024-09-19 22:12:43 +0200

    Update pkgs.R

--- pkgs.R
+++ pkgs.R
@@ -1,6 +1,6 @@
 df <- data.frame(
     package = c("dawaR"),
     url = c("https://github.com/aleksanderbl29/dawaR"),
-    subdir = c("universe-release")
+    branch = c("universe-release")
 )
 jsonlite::write_json(df, 'packages.json', pretty = TRUE)
