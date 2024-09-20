d08625126f3ad980499f6758f57e90375f1b09a0
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-09-20 17:24:19 +0200

    Add aleksandeR package

--- pkgs.R
+++ pkgs.R
@@ -2,7 +2,7 @@ df <- data.frame(
   package = c("dawaR",
               "aleksandeR"),
   url = c("https://github.com/aleksanderbl29/dawaR",
-          "https://github.com/aleksanderbl29/dawaR"),
+          "https://github.com/aleksanderbl29/aleksandeR"),
   branch = c("universe-release", NA)
 )
 jsonlite::write_json(df, "packages.json", pretty = TRUE)
