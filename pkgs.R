cf3387863f6cfbe8c354814b3355a70435f8012c
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-09-20 17:08:07 +0200

    Update package list

--- pkgs.R
+++ pkgs.R
@@ -1,6 +1,8 @@
 df <- data.frame(
-    package = c("dawaR"),
-    url = c("https://github.com/aleksanderbl29/dawaR"),
-    branch = c("universe-release")
+  package = c("dawaR",
+              "aleksandeR"),
+  url = c("https://github.com/aleksanderbl29/dawaR",
+          "https://github.com/aleksanderbl29/dawaR"),
+  branch = c("universe-release", NA)
 )
-jsonlite::write_json(df, 'packages.json', pretty = TRUE)
+jsonlite::write_json(df, "packages.json", pretty = TRUE)
