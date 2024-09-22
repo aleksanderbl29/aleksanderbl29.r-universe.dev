68a01678c5b45e31815b21f99260d69b73c40f1c
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-09-22 19:10:58 +0200

    make some of the json a func

--- pkgs.R
+++ pkgs.R
@@ -1,8 +1,17 @@
+pkg_url <- function(name) {
+  paste0("https://github.com/aleksanderbl29/", name)
+}
+
+packages <- c(
+  "dawaR",
+  "aleksandeR"
+)
+
+pkg_url(packages)
+
 df <- data.frame(
-  package = c("dawaR",
-              "aleksandeR"),
-  url = c("https://github.com/aleksanderbl29/dawaR",
-          "https://github.com/aleksanderbl29/aleksandeR"),
+  package = packages,
+  url = pkg_url(packages),
   branch = c("universe-release", NA)
 )
 jsonlite::write_json(df, "packages.json", pretty = TRUE)
