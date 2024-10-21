c04a5a5775b932c9e9da34212bef80d7310e4fa8
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-10-21 15:31:08 +0200

    Update packages.json with new packages and their URLs

--- pkgs.R
+++ pkgs.R
@@ -11,11 +11,18 @@ packages <- c(
   "aleksandeR"
 )
 
+ropengov_pkgs <- c(
+  "dawaR",
+  "geodk",
+  "dkdata"
+)
+
 pkg_url(packages)
 
 df <- data.frame(
-  package = c(packages, "geofi"),
-  url = c(pkg_url(packages), ropengov_pkg_url("geofi")),
-  branch = c("universe-release", NA, NA)
+  package = c(packages, ropengov_pkgs),
+  url = c(pkg_url(packages), ropengov_pkg_url(ropengov_pkgs)),
+  branch = c("universe-release",
+              rep("NA", length(ropengov_pkgs) + length(packages) - 1))
 )
 jsonlite::write_json(df, "packages.json", pretty = TRUE)
