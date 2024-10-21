08e6975cccdc547e56bfbe3151c24dc743d70ae0
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2024-10-21 15:18:38 +0200

    Add geofi package and update packages.json

--- pkgs.R
+++ pkgs.R
@@ -2,6 +2,10 @@ pkg_url <- function(name) {
   paste0("https://github.com/aleksanderbl29/", name)
 }
 
+ropengov_pkg_url <- function(name) {
+  paste0("https://github.com/ropengov/", name)
+}
+
 packages <- c(
   "dawaR",
   "aleksandeR"
@@ -10,8 +14,8 @@ packages <- c(
 pkg_url(packages)
 
 df <- data.frame(
-  package = packages,
-  url = pkg_url(packages),
-  branch = c("universe-release", NA)
+  package = c(packages, "geofi"),
+  url = c(pkg_url(packages), ropengov_pkg_url("geofi")),
+  branch = c("universe-release", NA, NA)
 )
 jsonlite::write_json(df, "packages.json", pretty = TRUE)
