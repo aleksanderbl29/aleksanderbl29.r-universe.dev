6a27862f1de915cefec9cd61e1dcfb19c5bc594a
aleksanderbl29 <73799306+aleksanderbl29@users.noreply.github.com>
2025-01-05 21:50:16 +0100

    add broomstick and options to include other peoples pkgs

--- pkgs.R
+++ pkgs.R
@@ -6,17 +6,26 @@ ropengov_pkg_url <- function(name) {
   paste0("https://github.com/ropengov/", name)
 }
 
-packages <- c(
+my_packages <- c(
   "dawaR",
   # "dkdatadist",
   "aleksandeR"
 )
 
+other_packages <- c(
+  "broomstick"
+)
+
+other_urls <- c(
+  "https://github.com/njtierney/broomstick"
+)
+
 pkg_url(packages)
 
 df <- data.frame(
-  package = packages,
-  url = pkg_url(packages),
-  branch = c("universe-release", rep(NA, length(packages) - 1))
+  package = c(my_packages, other_packages),
+  url = c(pkg_url(my_packages), other_urls),
+  branch = c("universe-release",
+    rep(NA, length(my_packages) + length(other_packages) - 1))
 )
 jsonlite::write_json(df, "packages.json", pretty = TRUE)
